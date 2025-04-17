1306c4fe2598ff15516ea1d6a034435f49d5bd72
yeudaborodyanski <yeudaborodyanski@gmail.com>
2025-04-17 18:51:14 +0300

    timou

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  api: {
+    bodyParser: false,
+    responseLimit: false,
+    externalResolver: true,
+  },
 };
 
 export default nextConfig;
