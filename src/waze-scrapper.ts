925a109e63571e21fa7ac8a8b201034755195589
yeudaborodyanski <yeudaborodyanski@gmail.com>
2025-04-17 20:33:12 +0300

    maybe

--- src/waze-scrapper.ts
+++ src/waze-scrapper.ts
@@ -3,7 +3,8 @@ import puppeteer from "puppeteer";
 import puppeteerCore from "puppeteer-core";
 
 export async function wazeScrapperV3(url: string) {
-    const isDev = !chromium.headless; // דרך נוחה לדעת שאתה לוקאלי
+    const isDev = process.env.DEV_MODE === "DEV" //!chromium.headless; // דרך נוחה לדעת שאתה לוקאלי
+    console.log(isDev)
     const browser = await (isDev ? puppeteer : puppeteerCore).launch({
         args: isDev ? ['--no-sandbox'] : chromium.args,
         defaultViewport: chromium.defaultViewport,
@@ -12,9 +13,9 @@ export async function wazeScrapperV3(url: string) {
     });
 
     const page = await browser.newPage();
-    await page.goto(url, { waitUntil: "networkidle2" });
+    await page.goto(url, {waitUntil: "networkidle2"});
 
-    await page.waitForSelector('h1.wm-poi-name-and-address__name', { timeout: 5000 })
+    await page.waitForSelector('h1.wm-poi-name-and-address__name', {timeout: 5000})
         .catch(() => console.warn('לא נמצא הסלקטור - ממשיך'));
 
 
@@ -27,3 +28,4 @@ export async function wazeScrapperV3(url: string) {
     await browser.close();
     return poiName;
 }
+
