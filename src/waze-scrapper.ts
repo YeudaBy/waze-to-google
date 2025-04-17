e21b6ddbb191c7124f9ac6c5ba2a7f874e06ebe9
yeudaborodyanski <yeudaborodyanski@gmail.com>
2025-04-17 18:48:32 +0300

    idfc

--- src/waze-scrapper.ts
+++ src/waze-scrapper.ts
@@ -1,48 +1,29 @@
 import chromium from "@sparticuz/chromium";
+import puppeteer from "puppeteer";
 import puppeteerCore from "puppeteer-core";
 
 export async function wazeScrapperV3(url: string) {
-    let browser = null;
-
-    // if (process.env.NODE_ENV === 'development') {
-    //     browser = await puppeteer.launch({
-    //         args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    //         headless: true,
-    //     });
-    // }
-    // if (process.env.NODE_ENV === 'production') {
-    browser = await puppeteerCore.launch({
-        args: [
-            ...chromium.args,
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--single-process'
-        ],
+    const isDev = !chromium.headless; // דרך נוחה לדעת שאתה לוקאלי
+    const browser = await (isDev ? puppeteer : puppeteerCore).launch({
+        args: isDev ? ['--no-sandbox'] : chromium.args,
         defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath(),
+        executablePath: isDev ? undefined : await chromium.executablePath(),
         headless: chromium.headless,
     });
-    // }
-    if (!browser) {
-        return
-    }
+
     const page = await browser.newPage();
-    await page.goto(url, {waitUntil: 'networkidle2'});
-    console.log('הדף נטען בהצלחה');
+    await page.goto(url, { waitUntil: "networkidle2" });
 
-    await page.waitForSelector('h1.wm-poi-name-and-address__name', {timeout: 50000})
-        .catch(() => {
-            console.error('לא נמצא הסלקטור בזמן ההמתנה - ממשיך בכל מקרה')
-        });
+    await page.waitForSelector('h1.wm-poi-name-and-address__name', { timeout: 5000 })
+        .catch(() => console.warn('לא נמצא הסלקטור - ממשיך'));
 
+
+    // @ts-expect-error some ts shit
     const poiName = await page.evaluate(() => {
-        const element = document.querySelector('h1.wm-poi-name-and-address__name');
-        return element ? element.innerHTML : 'לא נמצא אלמנט התואם לסלקטור';
+        const el = document.querySelector('h1.wm-poi-name-and-address__name');
+        return el ? el.textContent : 'לא נמצא אלמנט תואם';
     });
 
     await browser.close();
-
-    console.log('התוכן שנשלף:', poiName);
     return poiName;
 }
